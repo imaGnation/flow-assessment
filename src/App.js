db9edd3326bba7f3b6f2be62af1aa7c168df02dc
leboTsotsi <stifler.tsotsi>
2019-06-16 19:09:50 +0200

    conclude DatePicker & init api middleware

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './assets/bitcoin-image.png';
 import './App.scss';
+import DatePicker from './components/DatePicker';
+import moment from 'moment';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  constructor(props, state) {
+    super(props, state);
+
+    this.state = {
+      startDate: moment().add('M', -6),
+      endDate: moment()
+    }
+  }
+
+  onStardDateChange = (value) => {
+    this.setState({ startDate: value });
+  }
+
+  onEndDateChange = (value) => {
+    this.setState({ endDate: value });
+  }
+
+  render = () => {
+    return (<div className="App" >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
       </header>
-    </div>
-  );
+      <div className="Period-Container">
+        <DatePicker label="Start Date" value={this.state.startDate} onDateChange={(value) => this.onStardDateChange(value)} />
+        <DatePicker label="End Date" startDate={this.state.startDate} value={this.state.endDate} isEndDate={true} onDateChange={(value) => this.onEndDateChange(value)} />
+      </div>
+    </div>);
+  }
 }
 
 export default App;
