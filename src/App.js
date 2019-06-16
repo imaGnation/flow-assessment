f676c31ea78f4df2e862c718aafedd4a7bda5d93
leboTsotsi <stifler.tsotsi>
2019-06-16 22:27:14 +0200

    read and store values from api

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import React, { Component } from 'react';
-import logo from './assets/bitcoin-image.png';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
 import './App.scss';
-import DatePicker from './components/DatePicker';
-import moment from 'moment';
+import Layout from './components/Layout';
 
-class App extends Component {
-
-  constructor(props, state) {
-    super(props, state);
-
-    this.state = {
-      startDate: moment().add('M', -6),
-      endDate: moment()
-    }
-  }
-
-  onStardDateChange = (value) => {
-    this.setState({ startDate: value });
-  }
-
-  onEndDateChange = (value) => {
-    this.setState({ endDate: value });
-  }
-
-  render = () => {
-    return (<div className="App" >
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div className="Period-Container">
-        <DatePicker label="Start Date" value={this.state.startDate} onDateChange={(value) => this.onStardDateChange(value)} />
-        <DatePicker label="End Date" startDate={this.state.startDate} value={this.state.endDate} isEndDate={true} onDateChange={(value) => this.onEndDateChange(value)} />
-      </div>
-    </div>);
-  }
-}
-
-export default App;
+export default () => (
+  <Layout>
+    <Route exact path='/' component={Home} />
+  </Layout>
+);
